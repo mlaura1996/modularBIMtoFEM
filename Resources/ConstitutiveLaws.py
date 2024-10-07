54662d286db6828081ed7c8f2af0823434730726
Maria Laura Leonardi <99893051+mlaura1996@users.noreply.github.com>
2024-10-07 14:51:51 +0100

    Add files via upload

---
+++ Resources/ConstitutiveLaws.py
@@ -0,0 +1,273 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+# Constants
+TOLERANCE = 1.0e-12
+
+class ConstitutiveLaws:
+
+    class BezierCurve_Compression:
+
+        @staticmethod
+        def eval_bezier(xi, x0, x1, x2, y0, y1, y2):
+            A = x0 - 2.0 * x1 + x2
+            B = 2.0 * (x1 - x0)
+            C = x0 - xi
+            if abs(A) < TOLERANCE:
+                x1 += 1.0E-6 * (x2 - x0)
+                A = x0 - 2.0 * x1 + x2
+                B = 2.0 * (x1 - x0)
+                C = x0 - xi
+            if A == 0.0:
+                return 0.0
+            D = B * B - 4.0 * A * C
+            t = (-B + math.sqrt(D)) / (2.0 * A)
+            return (y0 - 2.0 * y1 + y2) * t * t + 2.0 * (y1 - y0) * t + y0
+
+        @staticmethod
+        def eval_area(x1, x2, x3, y1, y2, y3):
+            return x2 * y1 / 3.0 + x3 * y1 / 6.0 - x2 * y3 / 3.0 + x3 * y2 / 3.0 + x3 * y3 / 2.0 - x1 * (y1 / 2.0 + y2 / 3.0 + y3 / 6.0)
+        
+        @staticmethod
+        def calc_G(sp, sk, sr, ep, ej, ek, er, eu):
+            G1 = ep * sp / 2.0
+            G2 = ConstitutiveLaws.BezierCurve_Compression.eval_area(ep, ej, ek, sp, sp, sk)
+            G3 = ConstitutiveLaws.BezierCurve_Compression.eval_area(ek, er, eu, sk, sr, sr)
+            return G1 + G2 + G3
+
+        @staticmethod
+        def stretch(S, ep, ej, ek, er, eu):
+            ej += (ej - ep) * S
+            ek += (ek - ep) * S
+            er += (er - ep) * S
+            eu += (eu - ep) * S
+            return (ej, ek, er, eu)
+
+        @staticmethod
+        def getAllPoints(E, Gf, lch, s0, sp, sr, ep, c1, c2, c3):
+            Gf_bar = Gf / lch
+
+            # Auto-calculation of other parameters
+            sk = sr + (sp - sr) * c1
+            e0 = s0 / E
+            alpha = 2.0 * (ep - sp / E)
+            ej = ep + alpha * c2
+            ek = ej + alpha * (1 - c2)
+            if abs(sp - sk) > 1.0e-16:
+                er = (ek - ej) / (sp - sk) * (sp - sr) + ej
+            else:
+                er = ek * c3
+            eu = er * c3
+
+            # Regularization
+            G_bar = ConstitutiveLaws.BezierCurve_Compression.calc_G(sp, sk, sr, ep, ej, ek, er, eu)
+            G1 = sp * ep / 2.0
+            stretch_value = (Gf_bar - G1) / (G_bar - G1) - 1.0
+            if stretch_value <= -1.0:
+                raise ValueError(
+                    f"Fracture energy (Gf = {Gf}) is too small, or the element characteristic length (lch = {lch}) is too large.\n"
+                    f"Minimum Gf to avoid constitutive snap-back is {G1}\n"
+                )
+            ej, ek, er, eu = ConstitutiveLaws.BezierCurve_Compression.stretch(stretch_value, ep, ej, ek, er, eu) #ej, ek, er, eu
+
+            return ej, ek, er, eu, sk
+        
+        @staticmethod
+        def computeValues(E, s0, sp, sr, ep, ej, ek, er, eu, e0, sk, xi, R):
+            # Compute damage
+            if xi <= ep:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, e0, sp / E, ep, s0, sp, sp)
+            elif xi <= ek:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, ep, ej, ek, sp, sp, sk)
+            elif xi <= eu:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, ek, er, eu, sk, sr, sr)
+            else:
+                s = sr
+            D = 1.0 - s / R
+
+            # Derivative
+            h = max((sp - s0) / 100.0, 1.0e-8)
+            xi = (R + h) / E
+            if xi <= ep:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, e0, sp / E, ep, s0, sp, sp)
+            elif xi <= ek:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, ep, ej, ek, sp, sp, sk)
+            elif xi <= eu:
+                s = ConstitutiveLaws.BezierCurve_Compression.eval_bezier(xi, ek, er, eu, sk, sr, sr)
+            else:
+                s = sr
+            dch = 1.0 - s / (R + h)
+            dDdR = (dch - D) / h
+
+            return D, dDdR
+        
+        @staticmethod
+        def Compression(E, s0, sp, Gc, lch):
+            sr = 0.001        # Residual stress in Pascals (e.g., 50 MPa)
+            ep = 0.010
+            
+
+            c1 = 0.5         # Material parameter for intermediate stress
+            c2 = 0.5         # Material parameter for intermediate strain
+            c3 = 1.0         # Material parameter for residual strain
+
+            # Compute necessary points
+            ej, ek, er, eu, sk = ConstitutiveLaws.BezierCurve_Compression.getAllPoints(E, Gc, lch, s0, sp, sr, ep, c1, c2, c3)
+            e0 = s0 / E
+
+            #Initiate the vectors
+            Ce = [0, e0]
+            Cs = [0, s0]
+            Cd = [0, 0]
+
+            #First Bezier curve
+            strain_steps = np.linspace(e0, ep, 100)
+            for epsilon in strain_steps:
+                R = E * epsilon
+                xi = R / E
+                D, dDdR = ConstitutiveLaws.BezierCurve_Compression.computeValues(E, s0, sp, sr, ep, ej, ek, er, eu, e0, sk, xi, R)
+                stress = (1 - D) * R
+                Cs.append(stress)
+                Ce.append(epsilon)
+                Cd.append(D)
+
+            strain_steps = np.linspace(ep, ek, 100)
+
+            for epsilon in strain_steps:  
+                R = E * epsilon
+                xi = R / E
+                D, dDdR = ConstitutiveLaws.BezierCurve_Compression.computeValues(E, s0, sp, sr, ep, ej, ek, er, eu, e0, sk, xi, R)
+                stress = (1 - D) * R
+                Cs.append(stress)
+                Ce.append(epsilon)
+                Cd.append(D)
+
+            strain_steps = np.linspace(ek, eu, 100, False)
+
+            for epsilon in strain_steps:  
+                R = E * epsilon
+                xi = R / E
+                D, dDdR = ConstitutiveLaws.BezierCurve_Compression.computeValues(E, s0, sp, sr, ep, ej, ek, er, eu, e0, sk, xi, R)
+                stress = (1 - D) * R
+                Cs.append(stress)
+                Ce.append(epsilon)
+                Cd.append(D)
+
+            return(Ce, Cs, Cd)
+        
+        
+    
+    class ExponentialSoftening_Tension:
+
+        @staticmethod
+        def compute(E, Gf, lch, s0, R):
+            D = 0.0
+            dDdR = 0.0
+            
+            if R <= s0:
+                D = 0.0
+                dDdR = 0.0
+            else:
+                r0 = s0
+                lt = 2.0 * E * Gf / (s0 * s0)
+                if lch >= lt:
+                    raise ValueError(
+                        f"Fracture energy (Gf = {Gf}) is too small, "
+                        f"or the element characteristic length (lch = {lch}) is too large.\n"
+                        f"The maximum allowed lch is 2*E*Gf/(s0^2) = {lt} with:\n"
+                        f"E = {E}\nGf = {Gf}\ns0 = {s0}\n"
+                    )
+                Hs = lch / (lt - lch)
+                A = 2.0 * Hs
+                D = 1.0 - r0 / R * math.exp(A * (1.0 - R / r0))
+                dDdR = (r0 + A * R) / (R * R * math.exp(A * (R / r0 - 1.0)))
+                rmin = 1.0e-4 * s0
+                if (1.0 - D) * R < rmin:
+                    D = 1.0 - rmin / R
+                    dDdR = rmin / (R * R)
+            
+            return D, dDdR
+        
+        @staticmethod
+        def tension(E, s0, Gf, lch):
+            e0 = s0 / E
+            eu = 0.001
+
+            Te = [0, e0]
+            Ts = [0, s0]
+            Td = [0, 0]
+
+            strain_steps = np.linspace(e0, eu, 100)
+
+            for epsilon in strain_steps:
+                R = E * epsilon
+                D, dDdR = ConstitutiveLaws.ExponentialSoftening_Tension.compute(E, Gf, lch, s0, R)
+                stress = (1 - D) * R
+                Ts.append(stress)
+                Te.append(epsilon)
+                Td.append(D)
+
+            return(Te, Ts, Td)
+
+#Elastic properties
+E = 2550.0 #Magenes_2010
+v = 0.03 #Trasveral from Magenes 2010
+
+#Compressive properties properties
+s0 = 1.09 #Magenes
+sp = 3.28 #Magenes 
+ec = 0.030
+Gc = 4  #Ana Araujo
+
+#Tensile properties
+ft = 0.137
+Gt = 0.02 #Ana Araujo
+
+#Mesh size and fracture energy calibration
+meshSize = 100 
+
+# Compute the values
+Ce, Cs, Cd = ConstitutiveLaws.BezierCurve_Compression.Compression(E, s0, sp, Gc, meshSize)
+
+# Create the plots
+plt.figure(figsize=(12, 8))
+
+# Plot stress-strain curve
+plt.subplot(3, 1, 1)
+plt.plot(Ce, Cs, label='Stress-Strain')
+plt.xlabel('Strain')
+plt.ylabel('Stress (MPa)')
+plt.legend()
+plt.grid(True)
+
+import csv
+
+# Write data to CSV file
+with open('Prova.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['x', 'y'])  # Write header
+    writer.writerows(zip(Ce, Cs))  # Write data
+
+# Compute the values
+Te, Ts, Td = ConstitutiveLaws.ExponentialSoftening_Tension.tension(E, ft, Gt, meshSize)
+
+# Create the plots
+plt.figure(figsize=(12, 8))
+
+# Plot stress-strain curve
+plt.subplot(3, 1, 1)
+plt.plot(Ce, Cs, label='Stress-Strain')
+plt.xlabel('Strain')
+plt.ylabel('Stress (MPa)')
+plt.legend()
+plt.grid(True)
+
+import csv
+
+# Write data to CSV file
+with open('Prova2.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['x', 'y'])  # Write header
+    writer.writerows(zip(Te, Ts))  # Write data
+
+
---
+++ Resources/Mesh.py
@@ -0,0 +1,161 @@
+import gmsh
+import openseespy.opensees as ops
+
+np = ops.getNP()
+
+def createGmshModel(stepfile, labels):
+    gmsh.initialize()
+    gmsh.model.mesh.setOrder(2)
+    gmsh.option.setNumber("General.Terminal", 1)
+    gmsh.model.add(stepfile[-4])
+    gmsh.open(stepfile)
+    gmsh.model.occ.fragment(gmsh.model.occ.getEntities(3), [])
+    gmsh.model.occ.synchronize()
+    gmshmodel = createMatPhisicalGroups(gmsh.model, labels)
+
+    gmshmodel = fixBoundaries(gmsh.model)
+    # # for load in loads:
+    # #     gmshmodel = applyLoad(gmshmodel, load)
+    gmshmodel = meshing(gmshmodel, 300, False) #needs to be external
+    gmshmodel = paralleliseMesh(gmshmodel)
+    partitions = getElementInPartitions(gmshmodel)         
+    print(len(partitions))
+
+def createMatPhisicalGroups(gmshmodel, labels):
+    tridEleTags = gmshmodel.occ.getEntities(dim = 3)
+
+    OriginalDictionaryKeys = []
+    OriginalDictionaryValues = []
+    for ele in tridEleTags:
+        tg = ele[1]
+        EntName = gmshmodel.getEntityName(3, tg)
+        OriginalDictionaryKeys.append(EntName)
+        OriginalDictionaryValues.append(tg)
+
+    OriginalDictionary= {k: v for k, v in zip(OriginalDictionaryKeys, OriginalDictionaryValues)}
+
+    LabelDictionaryKeys = []
+    [LabelDictionaryKeys.append(x) for x in labels if x not in LabelDictionaryKeys]
+    LabelDictionaryValues = [0]*len(LabelDictionaryKeys)
+
+    LabelDictionary = {key: None for key in LabelDictionaryKeys}
+
+    FinalDict = {}
+
+    for key2 in LabelDictionary.keys():
+        matches = [OriginalDictionary[key1] for key1 in OriginalDictionary.keys() if key2 in key1]
+        if matches:
+            FinalDict[key2] = matches
+
+
+    for key in FinalDict.keys():
+        value = FinalDict[key]
+        gmshmodel.addPhysicalGroup(dim=3, tags=value, name=key)
+    return(gmsh.model)
+   
+def fixBoundaries(gmshmodel):
+    for dim, tag in gmshmodel.getPhysicalGroups():
+        if 'Footing' in gmshmodel.getPhysicalName(dim, tag):
+            footingTags = gmshmodel.getEntitiesForPhysicalGroup(3, tag)
+    footingDimTag = [(3, footingTags[i]) for i in range(len(footingTags))]
+    boundary = (gmshmodel.get_boundary(footingDimTag))
+    boundaryTags = [sublist[1] for sublist in boundary]
+    realBoundary  = [ent for ent in boundaryTags if ent < 0]
+    FinalBound  = [abs(n) for n in realBoundary]
+
+    ToFix = []
+    for surface in FinalBound:
+        # Get the nodes of the surface
+        normal = gmshmodel.getNormal(surface, [1,0,0,1])
+        if normal[2] == -1 and normal[5] ==-1:
+            ToFix.append(surface)
+
+    gmshmodel.addPhysicalGroup(dim=2, tags=ToFix, name="Fix")
+
+    return(gmsh.model)
+
+def applyLoad(gmshmodel, runGmsh = True, name=""): #in the BIM model I have to define the various types of load and then names 
+    for dim, tag in gmshmodel.getPhysicalGroups():
+    # get the name of the physical group
+        name = gmshmodel.getPhysicalName(dim, tag)
+
+    # check if the name is "Fix"
+    if "Loaded" in name:
+        # get the tags of all the entities in the physical group "Fix"
+        BoardTags = gmshmodel.getEntitiesForPhysicalGroup(3, tag)
+
+    BoardDimTag = [(3, BoardTags[i]) for i in range(len(BoardTags))]
+    boundary = (gmshmodel.get_boundary(BoardDimTag))
+    boundaryTags = [sublist[1] for sublist in boundary]
+    realBoundary  = [ent for ent in boundaryTags if ent < 0]
+    FinalBound  = [abs(n) for n in boundaryTags]
+
+    # Compute the normal of each surface
+    
+    forLoad = []
+    for surface in FinalBound:
+        normal = gmshmodel.getNormal(surface, [1,0,0,1])
+        print(str(surface) + str(normal))
+        if float(normal[2]) > 0 and float(normal[5]) > 0:
+            forLoad.append(surface)
+
+    gmshmodel.addPhysicalGroup(dim=2, tags=forLoad, name=name)
+
+    return(gmsh.model)
+
+def meshing(gmshmodel, meshSize, runGmsh = True): #attenzione che voglio fare un parametro ifc
+    gmshmodel.geo.removeAllDuplicates()
+
+    gmsh.option.setNumber("Mesh.AngleToleranceFacetOverlap", 0.001)
+    gmsh.option.setNumber("Mesh.MeshSizeMax", meshSize)
+
+    gmshmodel.mesh.generate(3)
+    gmshmodel.mesh.optimize()
+    gmshmodel.mesh.remove_duplicate_nodes()
+
+    if runGmsh:
+        gmsh.fltk.run()
+
+    return(gmsh.model)
+
+def paralleliseMesh(gmshmodel, partitioner=0, write_to_disk=1):
+
+    # Set options directly using function arguments
+    gmsh.option.setNumber("Mesh.PartitionCreateTopology", 1)
+    gmsh.option.setNumber("Mesh.PartitionCreateGhostCells", 0)
+    gmsh.option.setNumber("Mesh.PartitionCreatePhysicals", 0)
+    gmsh.option.setNumber("Mesh.PartitionOldStyleMsh2", 0)
+    gmsh.option.setNumber("Mesh.PartitionSplitMeshFiles", 0)
+
+    if partitioner == 0:
+        # Use Metis to create partitions
+        gmshmodel.mesh.partition(np)
+    else:
+        gmsh.plugin.setNumber("SimplePartition", "NumSlicesX", np)
+        gmsh.plugin.setNumber("SimplePartition", "NumSlicesY", 1)
+        gmsh.plugin.setNumber("SimplePartition", "NumSlicesZ", 1)
+        gmsh.plugin.run("SimplePartition")
+
+    # Save mesh file (or files, if `Mesh.PartitionSplitMeshFiles' is set):
+    if write_to_disk == 1:
+        gmsh.write("partitionedModel.msh")
+
+    return(gmsh.model)
+
+def getElementInPartitions(gmshmodel):
+
+    partition_groups = {}
+
+    entities = gmshmodel.getEntities()
+    for e in entities:
+        partitions = gmshmodel.getPartitions(e[0], e[1])
+        if len(partitions):
+            for partition in partitions:
+                if partition not in partition_groups:
+                    partition_groups[partition] = []
+                partition_groups[partition].append(e)
+    gmsh.fltk.run()
+    return(partition_groups)
+
+
+
